21c5f1c89121af460865b80e3e3f17d145516fb9
Mafer <marialopez@pacifiko.com>
2020-04-16 19:36:55 -0600

    Revert "Revert "HT2 Done""
    This reverts commit 43d548726f50ca8ce76103572077609d757e19b2.

--- linear_cost_derivate.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def linear_cost_derivate(X, y, theta, lamda ):
-    h = np.matmul(X, theta)
-    m, _ = X.shape
-    result = np.matmul((h - y).T, X).T / m
-    regularization = (lamda / m) * theta.sum()
-    return result + regularization
